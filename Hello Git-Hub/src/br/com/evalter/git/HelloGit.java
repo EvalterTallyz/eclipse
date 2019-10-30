9f6911dc1cd81afeff3ec37d871c8c109d5e743a
EvalterTallyz <37478571+EvalterTallyz@users.noreply.github.com>
2019-10-30 15:57:13 -0300

    Add arquivos via upload

---
+++ Hello Git-Hub/src/br/com/evalter/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.evalter.git;
+
+/**
+ * @author 11130659402
+ *Exemplo de interação do Git-Hub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Teste");
+
+	}
+
+}
